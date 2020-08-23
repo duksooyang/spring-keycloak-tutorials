28b12e81da4d401a2d07c8ac97b94ff910234ba7
Yang Duk Soo <sayduke@gmail.com>
2020-08-24 00:13:42 +0900

    adding codes

--- keycloak-spring-boot-first-look/src/main/java/com/thomasvitale/keycloak/controller/LibraryController.java
+++ keycloak-spring-boot-first-look/src/main/java/com/thomasvitale/keycloak/controller/LibraryController.java
@@ -1,13 +1,17 @@
 package com.thomasvitale.keycloak.controller;
 
 import com.thomasvitale.keycloak.repository.BookRepository;
+import org.keycloak.KeycloakPrincipal;
 import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Controller
 public class LibraryController {
@@ -39,6 +43,15 @@ public class LibraryController {
 		return "manager";
 	}
 
+	@GetMapping(value = "/userattr")
+	public String getUserAttribute(Model model) {
+		configCommonAttributes(model);
+		model.addAttribute("books", bookRepository.readAll());
+		return "userattr";
+	}
+
+
+
 	@GetMapping(value = "/logout")
 	public String logout() throws ServletException {
 		request.logout();
@@ -47,7 +60,31 @@ public class LibraryController {
 
 	private void configCommonAttributes(Model model) {
 		model.addAttribute("name", getKeycloakSecurityContext().getIdToken().getGivenName());
-		//model.addAttribute("name", getKeycloakSecurityContext().
+
+		AccessToken token = ((KeycloakPrincipal) request.getUserPrincipal()).getKeycloakSecurityContext().getToken();
+		Map<String, Object> otherClaims = token.getOtherClaims();
+		System.out.println(otherClaims.size());
+
+		//int cnt = 0;
+		//for(String key : otherClaims.keySet()){
+		//	cnt ++;
+		//	String value = String.valueOf(otherClaims.get(key));
+		//	model.addAttribute("attribute" + cnt, value);
+		//}
+
+
+		if(otherClaims.containsKey("fullname")){
+
+			model.addAttribute("attribute1", String.valueOf(otherClaims.get("fullname")));
+		}
+
+		if(otherClaims.containsKey("place")){
+			model.addAttribute("attribute2", String.valueOf(otherClaims.get("place")));
+		}
+
+		if(otherClaims.containsKey("sex")){
+			model.addAttribute("attribute3", String.valueOf(otherClaims.get("sex")));
+		}
 
 
 	}
@@ -55,4 +92,6 @@ public class LibraryController {
 	private KeycloakSecurityContext getKeycloakSecurityContext() {
 		return (KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName());
 	}
+
+
 }
